77f1e0c3f4ea8ee10a7435cb7cbfbe77e95ff63e
inkontoasty <142956737+inkontoasty@users.noreply.github.com>
2024-12-19 03:47:21 +0800

    Update cube.py

--- cube.py
+++ cube.py
@@ -66,15 +66,13 @@ def notate(move,cubee=cube):
 
 def scramble(): # fun fact this takes ~8.5 ms per scramble
     scram = []
-    dummycube = [[[face for x in range(3)] # the cube itself
-                for y in range(3)] # 6 x 3 x 3
-                for face in range(6)] # nobody needs numpy
+    reset()
     visited = []
     axis1, axis2 = -1, -2
     for n in range(30): # random + depth 1 check + axis check
         ok = [t+a for n,t in enumerate(TURNS) for a in ['',"'",'2'] if axis1!=axis2 or n//2!=axis1]
         for m in ok[:]:
-            c = notate(m,[[y[:] for y in f] for f in dummycube[:]])
+            c = notate(m,[[y[:] for y in f] for f in cube[:]])
             if c in visited:
                 ok.remove(m)
             else:
@@ -83,9 +81,36 @@ def scramble(): # fun fact this takes ~8.5 ms per scramble
             return scramble() # lol
         turn = random.choice(ok)
         scram.append(turn)
-        dummycube = notate(turn,dummycube)
+        notate(turn)
         axis1, axis2 = TURNS.index(turn[0])//2, axis1
+    reset()
     return scram
+
+try: # fix the a
+    import kociemba # oh well non built in things
+    # you dont want to see the code that generated these
+    JPERMS=[{48: 5, 45: 8, 2: 20, 9: 11, 10: 14}, {43: 52, 42: 51, 53: 17, 35: 33, 34: 30}, {30: 21, 27: 18, 24: 6, 38: 44, 41: 43}, {37: 19, 38: 20, 18: 9, 8: 6, 7: 3}, {23: 32, 26: 35, 29: 51, 17: 15, 16: 12}, {50: 30, 53: 27, 33: 24, 44: 42, 43: 39}, {7: 41, 6: 44, 38: 27, 24: 18, 21: 19}, {25: 43, 24: 42, 44: 53, 33: 27, 30: 28}, {19: 10, 20: 11, 9: 45, 2: 8, 5: 7}, {3: 50, 0: 53, 47: 33, 42: 36, 39: 37}, {34: 39, 33: 36, 42: 0, 47: 53, 50: 52}, {16: 25, 15: 24, 26: 44, 27: 29, 28: 32}, {32: 48, 35: 45, 51: 2, 11: 17, 14: 16}, {14: 34, 17: 33, 35: 42, 53: 51, 52: 48}, {39: 1, 36: 2, 0: 11, 45: 47, 46: 50}, {46: 37, 47: 38, 36: 18, 6: 0, 3: 1}, {5: 23, 8: 26, 20: 29, 15: 9, 12: 10}, {1: 14, 2: 17, 11: 35, 51: 45, 48: 46}, {10: 46, 11: 47, 45: 36, 0: 2, 1: 5}, {52: 16, 51: 15, 17: 26, 29: 35, 32: 34}, {28: 12, 29: 9, 15: 8, 20: 26, 23: 25}, {21: 3, 18: 0, 6: 47, 36: 38, 37: 41}, {12: 7, 9: 6, 8: 38, 18: 20, 19: 23}, {41: 28, 44: 29, 27: 15, 26: 24, 25: 21}]
+    def scramble(): # no sane me gonna implement 2 phase on my own
+        cubestr = [i for i in 'URFDLB' for j in range(9)] #umm its not str
+        for n in range(30):
+            for k,v in random.choice(JPERMS).items(): # yes random state means spamming Jb perms
+                cubestr[k],cubestr[v]=cubestr[v],cubestr[k] # swapping trick is so neat how i realised this only last week
+        solve = kociemba.solve(''.join(cubestr)).split(' ')[::-1] # gonna inverse the solve
+        scramcube = [[[ cubestr['URFDLB'.index(f)*9+y*3+x] 
+            for x in range(3) ] for y in range(3)] for f in TURNS]
+        reset()
+        for n,i in enumerate(solve):
+            notate(i)
+            if cube == scramcube: # hmm empty scramble?
+                return solve[:n]
+            solve[n] = i[0]+("'" if len(i)==1 else '' if i[-1]=="'" else '2')
+        reset()
+        return solve
+except:
+    print("To use random state scrambles instead of random move scrambles, "
+        "please install the python kociemba package:\n"
+        "python3 -m pip install kociemba\n"
+        "or visit https://pypi.org/project/kociemba for more information")
 
 OK, PLUS2, DNF = 0,1,2
 
@@ -144,4 +169,3 @@ def loadfile(filename):
     con.commit()
 
 fmtsecs = lambda s:f'{s//86400}d {(s%86400)//3600}h {(s%3600)//60}m {s%60}s'
-
